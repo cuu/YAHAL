76229404ea5f3ee8c98d6724ade27a05ad3b8942
Andreas Terstegge <terstegge@fh-aachen.de>
2017-01-12 09:52:49 +0100

    Some fixes for first version to work.

--- build/Target-msp432.cmake
+++ build/Target-msp432.cmake
@@ -9,8 +9,8 @@ set(CMAKE_SYSTEM_PROCESSOR msp432)
 set(toolchain_path   /usr/local/gcc-arm-none-eabi-6_2-2016q4)
 set(toolchain_prefix arm-none-eabi)
 
-set(CMAKE_C_COMPILER  (${toolchain_path}/bin/${toolchain_prefix}-gcc GNU)
-set(CMAKE_CXX_COMPILER(${toolchain_path}/bin/${toolchain_prefix}-g++ GNU)
+set(CMAKE_C_COMPILER   ${toolchain_path}/bin/${toolchain_prefix}-gcc)
+set(CMAKE_CXX_COMPILER ${toolchain_path}/bin/${toolchain_prefix}-g++)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
@@ -29,6 +29,7 @@ set(GCC_ARCH_FLAGS  "-mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -m
 set(GCC_DEBUG_FLAGS "-g -gdwarf-3 -gstrict-dwarf")
 set(GCC_MISC_FLAGS  "-Wall")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${GCC_ARCH_FLAGS} ${GCC_DEBUG_FLAGS} ${GCC_MISC_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=nosys.specs")
 
 add_definitions(-D__MSP432P401R__ -DTARGET_IS_FALCON -Dgcc)
 
