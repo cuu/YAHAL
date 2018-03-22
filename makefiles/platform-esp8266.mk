dca72ec1530c6b3132177581c468c47521fba213
student <student@mct-kurs>
2018-03-22 12:17:48 +0100

    Small updates

--- makefiles/platform-esp8266.mk
+++ makefiles/platform-esp8266.mk
@@ -14,14 +14,14 @@ TOOLCHAIN_PATH   = $(ESP8266_PACKAGE)/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404
 TOOLCHAIN_PREFIX = xtensa-lx106-elf
 
 # Various path variables
-ESP_SRC_DIR     = $(ESP8266_PACKAGE)/hardware/esp8266/2.3.0
+ESP_SRC_DIR     = $(ESP8266_PACKAGE)/hardware/esp8266/2.4.1
 ESP_CORE_DIR    = $(ESP_SRC_DIR)/cores/esp8266
 ESP_LIB_DIR     = $(ESP_SRC_DIR)/libraries
 ESP_VARIANTS_DIR= $(ESP_SRC_DIR)/variants
 ESP_SDK_DIR     = $(ESP_SRC_DIR)/tools/sdk
 
 # Various ESP tools 
-ESP_TOOL        = $(ESP8266_PACKAGE)/tools/esptool/0.4.9/esptool
+ESP_TOOL        = $(ESP8266_PACKAGE)/tools/esptool/0.4.13/esptool
 MKSPIFFS        = $(ESP8266_PACKAGE)/tools/mkspiffs/0.1.2/mkspiffs
 ESP_BOOTLOADER  = $(ESP_SRC_DIR)/bootloaders/eboot/eboot.elf
 
@@ -39,11 +39,14 @@ FLAGS_C        += -Wl,-EL -fno-inline-functions -nostdlib -std=gnu99
 FLAGS_ASM       = -x assembler-with-cpp
 
 FLAGS_LD        = -nostdlib -Wl,--no-check-sections -Wl,-static -Wl,--gc-sections 
-FLAGS_LD       += -Wl,-wrap,system_restart_local -Wl,-wrap,register_chipv6_phy -u call_user_start
+FLAGS_LD       += -Wl,-wrap,system_restart_local -Wl,-wrap,spi_flash_read 
+FLAGS_LD       += -u call_user_start -u _printf_float -u _scanf_float
 FLAGS_LD       += -T$(ESP_FLASH_SCRIPT)
 FLAGS_LD       += -Wl,-Map,$(basename $(TARGET)).map
 FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/lib$(QUOTE)
 FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/ld$(QUOTE)
+FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/libc/xtensa-lx106-elf/lib$(QUOTE)
+
 
 # spiffs stuff
 SPIFFS_FILES    = $(wildcard $(SPIFFS_DIR)/*)
@@ -68,19 +71,19 @@ CXXFLAGS = $(FLAGS_F) $(FLAGS_M) $(FLAGS_DEBUG) $(FLAGS_WARN) $(FLAGS_OPT) $(FLA
 CFLAGS   = $(FLAGS_F) $(FLAGS_M) $(FLAGS_DEBUG) $(FLAGS_WARN) $(FLAGS_OPT) $(FLAGS_C)
 ASMFLAGS = $(FLAGS_M) $(FLAGS_DEBUG) $(FLAGS_ASM)
 LDFLAGS  = $(FLAGS_DEBUG) $(FLAGS_WARN) $(FLAGS_OPT) $(FLAGS_LD)
-LIBS     = -lm -lgcc -lhal -lphy -lpp -lnet80211 -lwpa -lcrypto -lmain \
-           -lwps -laxtls -lsmartconfig -lmesh -lwpa2 -llwip_gcc -lstdc++
+LIBS     = -lhal -lphy -lpp -lnet80211 -llwip2 -lwpa -lcrypto -lmain -lwps \
+           -laxtls -lespnow -lsmartconfig -lairkiss -lwpa2 -lstdc++ -lm -lc -lgcc
 
 # Compiler defines
 ##################
-DEFINES  = -D__ets__ -DICACHE_FLASH -U__STRICT_ANSI__ -DF_CPU=80000000L -DLWIP_OPEN_SRC -DARDUINO=10609
-DEFINES += -DARDUINO_ESP8266_ESP01 -DARDUINO_ARCH_ESP8266 -DARDUINO_BOARD=\"ESP8266_ESP01\" -DESP8266 
+DEFINES  = -D__ets__ -DICACHE_FLASH -U__STRICT_ANSI__ -DF_CPU=80000000L -DLWIP_OPEN_SRC -DTCP_MSS=536 -DARDUINO=10805
+DEFINES += -DARDUINO_ESP8266_GENERIC -DARDUINO_ARCH_ESP8266 -DARDUINO_BOARD=\"ESP8266_GENERIC\" -DESP8266 
 #DEFINES += -DNDEBUG
 
 # Platform includes
 ###################
 PLATFORM_INC_DIRS  = $(ESP_SDK_DIR)/include
-PLATFORM_INC_DIRS += $(ESP_SDK_DIR)/lwip/include
+PLATFORM_INC_DIRS += $(ESP_SDK_DIR)/lwip2/include
 PLATFORM_INC_DIRS += $(ESP_VARIANTS_DIR)/generic
 
 # Additional rules (e.g. for upload)
