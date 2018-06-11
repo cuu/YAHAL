214395583c4f283c629e9e6dc8976478822944f4
Philipp Dreßen <philipp.dressen@rwth-aachen.de>
2018-06-11 11:34:19 +0200

    Added bus voltage flag treatment

--- src/driver/ina219_drv.cpp
+++ src/driver/ina219_drv.cpp
@@ -42,13 +42,15 @@ float ina219_drv::get_shunt_voltage(){
 
 float ina219_drv::get_bus_voltage(){
     uint16_t raw = read_u16(INA219::REG_BUS_V);
-    bool ovf = raw & 0b1;
-    bool cnvr = raw & 0b10;
+    bool ovf = raw & 0b1;       // overflow flag
+    //bool cnvr = raw & 0b10;     // conversion ready flag
+
+    if (ovf)
+        return 0;   // return 0 if overflow occured
 
     // eliminate flags
     float result = raw >> 3;
-    result *= 4;    // mV
-    result /= 1000;
+    result /= (1000. / 250);    // LSB = 4 mV
     return result;
 }
 void ina219_drv::writeRegister16(uint8_t reg, uint16_t value){
