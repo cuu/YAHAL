d4a0e9300d362508adcbbbc93c5a3fb1de447a1d
Andrea Tertegge <Tertegge@fh-aachen.de>
2019-03-19 17:53:59 +0100

    Updates to soft_i2c_slave example.

--- examples/msp432/example_soft_i2c/main.cpp
+++ examples/msp432/example_soft_i2c/main.cpp
@@ -15,7 +15,7 @@
 //  implementations of a I2C master and a I2C slave
 //  (soft_i2c_master and soft_i2c_slave).
 //  To successfully run the example, you have to
-//  connect the following GPIOs with a jumper:
+//  connect the following GPIOs with two jumpers:
 //
 //     - P6.4 and P5.5 (the SDA lines)
 //     - P6.5 and P5.4 (the SCL lines)
@@ -29,29 +29,27 @@
 
 #include <cstdint>
 #include <cstring>
-#include <cstdio>
-
 #include "uart_msp432.h"
-#include "std_io.h"
 #include "gpio_msp432.h"
 #include "i2c_msp432.h"
 #include "soft_i2c_master.h"
 #include "i2c_device.h"
+#include "String.h"
 
 // Simple us-delay (assuming 48MHz clock)
 void delay(uint32_t us) {
-    for (uint32_t i=0; i < 3*us; ++i);
+    for (uint32_t i=0; i < 2*us; ++i);
 }
 
 int main(void)
 {
-    // Redirect stdout to our backchannel UART
+    // Use UART for text output
     uart_msp432 uart;
-    std_io::inst.redirect_stdout(uart);
 
-    puts("Software I2C master/slave test");
-    puts("==============================");
-    puts("Press Button S1 during startup to select I2C master EUSCI_B1!");
+    uart.puts("\r\n\n");
+    uart.puts("Software I2C master/slave test\r\n");
+    uart.puts("==============================\r\n");
+    uart.puts("Press Button S1 during startup to select HW I2C master!\r\n");
 
     // set up gpios for button and I2C master/slave
     gpio_msp432_pin button1( PORT_PIN(1,1));
@@ -66,10 +64,10 @@ int main(void)
     i2c_interface * master = nullptr;
     if (button1.gpioRead() == LOW) {
         // button pressed -> use hardware master
-        puts("Using HW I2C master EUSCI_B1\n");
+        uart.puts("Using HW I2C master (EUSCI_B1)\r\n");
         master = new i2c_msp432(EUSCI_B1);
     } else {
-        puts("Using SW I2C master\n");
+        uart.puts("Using SW I2C master\r\n");
         master = new soft_i2c_master(m_sda, m_scl, delay, true);
     }
     master->setSpeed(20000);
@@ -80,56 +78,40 @@ int main(void)
 
     // Check the ID register
     ////////////////////////
-    int16_t ret;
     uint8_t buffer_send[257];
     uint8_t buffer_recv[256];
     buffer_send[0] = 0;
-    ret = master->i2cWrite(0x55, buffer_send, 1, false);
-    ret = master->i2cRead (0x55, buffer_recv, 2);
+    uart.puts("Trying to read ID register... ");
+    master->i2cWrite(0x55, buffer_send, 1, false);
+    master->i2cRead (0x55, buffer_recv, 2);
     if (buffer_recv[0] != 0xab || buffer_recv[1] != 0xcd) {
-        puts("Reading ID register failed.");
-        puts("Did you use the jumpers to connect master and slave?");
+        uart.puts("FAILED!\r\n");
+        uart.puts("Did you use the jumpers to connect master and slave?");
         while(1);
     }
+    uart.puts("OK!\r\n");
 
     // Send and receive data
     ////////////////////////
     for (uint16_t val=0; val < 256; ++val) {
-        puts("Send and Receive 256 bytes .... ");
         // Fill the send buffer
         buffer_send[0] = 1; // Send to register 1
-        for(uint16_t i=1; i <= 256; ++i) buffer_send[i] =val;
+        for(uint16_t i=1; i <= 256; ++i) buffer_send[i] = val;
         // Send the data
-        ret = master->i2cWrite(0x55, buffer_send, 257);
+        String msg = "Sending 256 bytes (0x" + to_String(val, 16) + ")...";
+        uart.puts(msg.c_str());
+        master->i2cWrite(0x55, buffer_send, 257);
         // Read back the data
-        ret = master->i2cRead(0x55, buffer_recv, 256);
+        uart.puts("Reading back...");
+        master->i2cRead(0x55, buffer_recv, 256);
         for(uint16_t i=0; i < 256; ++i) {
             if (buffer_recv[i] != val) {
-                puts("FAILURE");
+                uart.puts("FAILURE");
                 while(1);
             }
         }
-        puts("...OK!");
+        uart.puts(" OK!\r");
     }
-
-
-//    for(int i=0; i < 256; ++i) buffer[i] = i;
-//
-//
-//    const char *url = "\x01http"; //://www.web.de";
-//
-//    int16_t ret;
-//    ret = master.i2cWrite(0x55, (uint8_t*)url, strlen(url), true);
-//
-//    const char *url1 = "\x01Hallo";
-//
-//    ret = master.i2cWrite(0x55, (uint8_t*)url, strlen(url), false);
-//    ret = master.i2cWrite(0x55, (uint8_t*)url1, strlen(url1), true);
-//
-//
-//    uint8_t buffer[50];
-//    ret = master.i2cRead(0x55, buffer, 40);
-
-    while(1);
+    uart.puts("\n\nEnd of Test\r\n");
     return 0;
 }
