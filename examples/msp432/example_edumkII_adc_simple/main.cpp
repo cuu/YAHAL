2da54539ffe68c1ec93c7cdd231c2bbaa87c7426
Andreas Terstegge <Terstegge@fh-aachen.de>
2020-04-14 12:00:08 +0200

    Added UART output to ADC simple example.

--- examples/msp432/example_edumkII_adc_simple/main.cpp
+++ examples/msp432/example_edumkII_adc_simple/main.cpp
@@ -16,13 +16,21 @@
 // simple reading of single ADC conversions.
 //
 // The program will switch on the green/red LED on the
-// Launchpad when the Joystick is moved to left/right
+// Launchpad when the Joystick is moved to left/right.
+// The raw ADC values are shown on the backchannel UART.
 
 #include "gpio_msp432.h"
 #include "adc14_msp432.h"
+#include "uart_msp432.h"
+#include "std_io.h"
+#include "stdio.h"
 
 int main()
 {
+    // Redirect stdout so printf is working
+    uart_msp432 uart;
+    std_io::inst.redirect_stdout(uart);
+
     // GPIO Pins with LEDs
     gpio_msp432_pin red_led  (PORT_PIN(2, 0));
     gpio_msp432_pin green_led(PORT_PIN(2, 1));
@@ -30,7 +38,8 @@ int main()
     green_led.gpioMode(GPIO::OUTPUT);
 
     // ADC channel 15 is joystick X-movement.
-    // Convert voltage to with 10 bits resolution.
+    // Convert voltage range 0...3,3V to
+    // integer range 0..1023 (10 bits resolution).
 	adc14_msp432_channel joy_X(15);
 	joy_X.adcMode(ADC::ADC_10_BIT);
 
@@ -46,5 +55,7 @@ int main()
 	    // switch on red LED when Joystick is moved to the right
 	    bool right = (val - offset) > 200;
 	    red_led.  gpioWrite( right );
+
+	    printf("%d\n", val);
 	}
 }
