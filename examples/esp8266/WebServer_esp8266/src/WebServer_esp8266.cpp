ce1ac6a957a784c0bed6320537a8cb79db5a6b66
Andreas Terstegge <Terstegge@fh-aachen.de>
2020-05-27 00:13:35 +0200

    Changes due to new ESP8266 Arduino version.

--- examples/esp8266/WebServer_esp8266/src/WebServer_esp8266.cpp
+++ examples/esp8266/WebServer_esp8266/src/WebServer_esp8266.cpp
@@ -39,8 +39,8 @@ void drawGraph();
 void handleRoot();
 void handleNotFound();
 
-const char *ssid = "xxxx";
-const char *password = "xxxx";
+const char *ssid = "TG WLAN";
+const char *password = "Wanja&Mikesch2007";
 
 ESP8266WebServer server(80);
 
--- makefiles/platform-esp8266.mk
+++ makefiles/platform-esp8266.mk
@@ -10,19 +10,19 @@ ESP8266_PACKAGE  = $(HOME_DIR)/AppData/Local/Arduino15/packages/esp8266
 endif
 
 # Toolchain helpers
-TOOLCHAIN_PATH   = $(ESP8266_PACKAGE)/tools/xtensa-lx106-elf-gcc/2.5.0-3-20ed2b9
+TOOLCHAIN_PATH   = $(ESP8266_PACKAGE)/tools/xtensa-lx106-elf-gcc/2.5.0-4-b40a506
 TOOLCHAIN_PREFIX = xtensa-lx106-elf
 
 # Various path variables
-ESP_SRC_DIR     = $(ESP8266_PACKAGE)/hardware/esp8266/2.5.0
+ESP_SRC_DIR     = $(ESP8266_PACKAGE)/hardware/esp8266/2.7.1
 ESP_CORE_DIR    = $(ESP_SRC_DIR)/cores/esp8266
 ESP_LIB_DIR     = $(ESP_SRC_DIR)/libraries
 ESP_VARIANTS_DIR= $(ESP_SRC_DIR)/variants
 ESP_SDK_DIR     = $(ESP_SRC_DIR)/tools/sdk
 
 # Various ESP tools 
-ESP_TOOL        = $(ESP8266_PACKAGE)/tools/esptool/2.5.0-3-20ed2b9/esptool
-MKSPIFFS        = $(ESP8266_PACKAGE)/tools/mkspiffs/0.2.0/mkspiffs
+PYTHON          = $(ESP8266_PACKAGE)/tools/python3/3.7.2-post1/python3
+MKSPIFFS        = $(ESP8266_PACKAGE)/tools/mkspiffs/2.5.0-4-b40a506/mkspiffs
 ESP_BOOTLOADER  = $(ESP_SRC_DIR)/bootloaders/eboot/eboot.elf
 
 # Flag helper variables
@@ -43,16 +43,33 @@ FLAGS_LD       += -Wl,-wrap,system_restart_local -Wl,-wrap,spi_flash_read
 FLAGS_LD       += -u call_user_start -u _printf_float -u _scanf_float
 FLAGS_LD       += -T$(ESP_FLASH_SCRIPT)
 FLAGS_LD       += -Wl,-Map,$(basename $(TARGET)).map
-FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/lib$(QUOTE)
+FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/lib$(QUOTE)  
+FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/lib/NONOSDK22x_190703$(QUOTE)  
 FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/ld$(QUOTE)
 FLAGS_LD       += -L$(BUILD_DIR)
 FLAGS_LD       += -L$(QUOTE)$(ESP_SDK_DIR)/libc/xtensa-lx106-elf/lib$(QUOTE)
 
+# Compiler defines
+##################
+DEFINES  = -D__ets__
+DEFINES += -DICACHE_FLASH
+DEFINES += -U__STRICT_ANSI__
+DEFINES += -DNONOSDK22x_190703=1
+DEFINES += -DF_CPU=80000000L
+DEFINES += -DLWIP_OPEN_SRC -DTCP_MSS=536 -DLWIP_FEATURES=1 -DLWIP_IPV6=0 
+DEFINES += -DARDUINO=10808
+DEFINES += -DARDUINO_ESP8266_GENERIC
+DEFINES += -DARDUINO_ARCH_ESP8266
+DEFINES += -DARDUINO_BOARD=\"ESP8266_GENERIC\"
+DEFINES += -DESP8266
+#DEFINES += -DNDEBUG
 
-# spiffs stuff
-SPIFFS_FILES    = $(wildcard $(SPIFFS_DIR)/*)
-SPIFFS_BIN      = $(BUILD_DIR)/spiffs.bin
-
+# Platform includes
+###################
+PLATFORM_INC_DIRS  = $(ESP_SDK_DIR)/include
+PLATFORM_INC_DIRS += $(ESP_SDK_DIR)/lwip2/include
+PLATFORM_INC_DIRS += $(ESP_VARIANTS_DIR)/generic
+PLATFORM_INC_DIRS += $(ESP_SDK_DIR)/libc/xtensa-lx106-elf/include 
 
 #################################################
 # The following variables are used by common.mk #
@@ -75,19 +92,6 @@ LDFLAGS  = $(FLAGS_DEBUG) $(FLAGS_WARN) $(FLAGS_OPT) $(FLAGS_LD)
 LIBS     = -lhal -lphy -lpp -lnet80211 -llwip2-536-feat -lwpa -lcrypto -lmain -lwps \
            -lbearssl -laxtls -lespnow -lsmartconfig -lairkiss -lwpa2 -lstdc++ -lm -lc -lgcc
 
-# Compiler defines
-##################
-DEFINES  = -D__ets__ -DICACHE_FLASH -U__STRICT_ANSI__ -DF_CPU=80000000L
-DEFINES += -DLWIP_OPEN_SRC -DTCP_MSS=536 -DLWIP_FEATURES=1 -DLWIP_IPV6=0 
-DEFINES += -DARDUINO=10808 -DARDUINO_ESP8266_GENERIC -DARDUINO_ARCH_ESP8266 -DARDUINO_BOARD=\"ESP8266_GENERIC\" -DESP8266 
-#DEFINES += -DNDEBUG
-
-# Platform includes
-###################
-PLATFORM_INC_DIRS  = $(ESP_SDK_DIR)/include
-PLATFORM_INC_DIRS += $(ESP_SDK_DIR)/lwip2/include
-PLATFORM_INC_DIRS += $(ESP_VARIANTS_DIR)/generic
-
 # Add linker script source file
 LINK_DEPS += $(BUILD_DIR)/local.eagle.app.v6.common.ld
 
@@ -100,29 +104,35 @@ $(BUILD_DIR)/local.eagle.app.v6.common.ld : $(ESP_SDK_DIR)/ld/eagle.app.v6.commo
 	@echo "LDS $$(notdir $$<)"
 	$(HIDE) $(CC) -CC -E -P -DVTABLES_IN_FLASH $$< -o $$@
 
+# Generate SPIFFS binary
+$(BUILD_DIR)/spiffs.bin: $(wildcard $(SPIFFS_DIR)/*) 
+	@echo "Creating SPIFFS image $(SPIFFS_BIN)"
+	$(HIDE) $(MKDIR) $(BUILD_DIR) $(ERRIGNORE)
+	$(HIDE) $(MKSPIFFS) -c $(SPIFFS_DIR)      -b $(SPIFFS_BLOCKSIZE) \
+	                    -p $(SPIFFS_PAGESIZE) -s $(SPIFFS_SIZE) $$@
+
 .PHONY: upload
 upload: $$(TARGET)
 	@echo "Uploading programmer"
 	$(HIDE) make -C ../../msp432+wifiTick/WifiTick-Programmer upload
 	@echo "Uploading program"
-	$(HIDE) $(ESP_TOOL) -eo $(ESP_BOOTLOADER) \
-			    -bo $$(TARGET).bin \
-			    -bm $(ESP_FLASH_MODE) -bf $(ESP_FLASH_FREQ) -bz $(ESP_FLASH_SIZE) \
-			    -bs .text -bp 4096 -ec -eo $$(TARGET) \
-			    -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec
-	$(HIDE) $(ESP_TOOL) -v -cd $(ESP_RESET_MODE) -cp $(ESP_PORT) -cb $(ESP_BAUD) \
-			       -ca 0x000000 -cf $$(TARGET).bin
-
-$(SPIFFS_BIN): $(SPIFFS_FILES)
-	@echo "Creating SPIFFS image $(SPIFFS_BIN)"
-	$(HIDE) $(MKDIR) $(BUILD_DIR) $(ERRIGNORE)
-	$(HIDE) $(MKSPIFFS) -c $(SPIFFS_DIR) \
-			    -b $(SPIFFS_BLOCKSIZE) -p $(SPIFFS_PAGESIZE) \
-			    -s $(SPIFFS_SIZE) $(SPIFFS_BIN)
+	$(HIDE) $(PYTHON) $(ESP_SRC_DIR)/tools/elf2bin.py --eboot $(ESP_BOOTLOADER) \
+	--app $$(TARGET) --flash_mode dout --flash_freq 40 --flash_size 4M          \
+	--path $(TOOLCHAIN_PATH)/bin --out $$(TARGET).bin
+	$(HIDE) $(PYTHON) $(ESP_SRC_DIR)/tools/upload.py     \
+	--chip esp8266 --port $(ESP_PORT) --baud $(ESP_BAUD) --after no_reset\
+	write_flash 0x0 $$(TARGET).bin
 
 .PHONY: spiffs_upload
 spiffs_upload: $(SPIFFS_BIN)
-	@echo "Uploading SPIFFS image $(SPIFFS_BIN)"
-	$(HIDE) $(ESP_TOOL) -v -cd $(ESP_RESET_MODE) -cp $(ESP_PORT) -cb $(ESP_BAUD) \
-			       -ca $(SPIFFS_START)  -cf $(SPIFFS_BIN)
+	$(HIDE) $(PYTHON) $(ESP_SRC_DIR)/tools/upload.py     \
+	--chip esp8266 --port $(ESP_PORT) --baud $(ESP_BAUD) --after no_reset \
+	write_flash $(SPIFFS_START) $(BUILD_DIR)/spiffs.bin --end 
+
+.PHONY: erase_flash
+erase_flash: $(SPIFFS_BIN)
+	$(HIDE) $(PYTHON) $(ESP_SRC_DIR)/tools/upload.py     \
+	--chip esp8266 --port $(ESP_PORT) --baud $(ESP_BAUD) --after no_reset \
+	erase_flash --end 
+
 endef
