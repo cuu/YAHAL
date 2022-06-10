730fd0479e4a0acef66a9edd638fd57fbf24be06
Andreas Terstegge <Terstegge@fh-aachen.de>
2022-06-10 15:59:08 +0200

    Neues Beispiel für sin-wave Erzeugung mit BOOSTXL Audio boosterpack.

---
+++ examples/msp432/example_boostxl_audio_sin_wave/Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../audio_output.cpp \
+../main.cpp 
+
+LDS_SRCS += \
+../msp432p401r.lds 
+
+C_SRCS += \
+../startup_msp432p401r_gcc.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./startup_msp432p401r_gcc.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./audio_output.o \
+./main.o \
+./startup_msp432p401r_gcc.o \
+./system_msp432p401r.o 
+
+CPP_DEPS += \
+./audio_output.d \
+./main.d 
+
+OBJS__QUOTED += \
+"audio_output.o" \
+"main.o" \
+"startup_msp432p401r_gcc.o" \
+"system_msp432p401r.o" 
+
+C_DEPS__QUOTED += \
+"startup_msp432p401r_gcc.d" \
+"system_msp432p401r.d" 
+
+CPP_DEPS__QUOTED += \
+"audio_output.d" \
+"main.d" 
+
+CPP_SRCS__QUOTED += \
+"../audio_output.cpp" \
+"../main.cpp" 
+
+C_SRCS__QUOTED += \
+"../startup_msp432p401r_gcc.c" \
+"../system_msp432p401r.c" 
+
+
