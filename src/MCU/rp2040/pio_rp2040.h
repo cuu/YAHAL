f586bd2e62a2617bc1d14d9f76f509b6785f7a1b
Andreas Terstegge <Terstegge@fh-aachen.de>
2023-05-19 18:22:19 +0200

    Fixed bug with multiple ws2812 state machines -> Added method to set single bits in a SM register.

--- src/MCU/rp2040/pio_rp2040.h
+++ src/MCU/rp2040/pio_rp2040.h
@@ -247,6 +247,30 @@ struct SM {
         if (en) enable();
     }
 
+    void setRegisterBit(out_dest_t reg, uint8_t bit) {
+        // Store current configuration
+        bool en = isEnabled();
+        disable();
+        uint8_t out_base  = regs.SM_PINCTRL.OUT_BASE;
+        uint8_t out_count = regs.SM_PINCTRL.OUT_COUNT;
+        // Write only 1 bit
+        regs.SM_PINCTRL.OUT_BASE  = bit;
+        regs.SM_PINCTRL.OUT_COUNT = 1;
+        // Empty the TX Fifo
+        while(pio.FLEVEL & (0xf << (sm_index << 3))) {
+            execute( op_PULL(0, 0) );
+        }
+        // Write value to TX Fifo first.
+        // Then pull it and write it to destination.
+        writeTxFifo(1);
+        execute( op_PULL(0, 0)      );
+        execute( op_OUT (0, reg, 0) );
+        // Restore original state
+        regs.SM_PINCTRL.OUT_BASE  = out_base;
+        regs.SM_PINCTRL.OUT_COUNT = out_count;
+        if (en) enable();
+    }
+
     void setClock(uint32_t hz) {
         regs.SM_CLKDIV.INT = 125000000 / hz;
         uint64_t frac = ((125000000 % hz) << 8);
