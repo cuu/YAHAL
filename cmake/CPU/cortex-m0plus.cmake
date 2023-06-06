2a94b42c6763b5e5ecc6dd8fe294960dd3d0544e
Andreas Terstegge <Terstegge@fh-aachen.de>
2023-06-06 08:06:36 +0200

    Fix to the last fix ...

--- cmake/CPU/cortex-m0plus.cmake
+++ cmake/CPU/cortex-m0plus.cmake
@@ -6,6 +6,7 @@ set(YAHAL_CPU "cortex-m0plus")
 
 # Architecture flags
 add_compile_options(-mcpu=cortex-m0plus -march=armv6s-m -mthumb)
+add_link_options(-mcpu=cortex-m0plus -march=armv6s-m -mthumb)
 
 # Math flags 
 add_compile_definitions(ARM_MATH_CM0PLUS)
