6a71a273ce6604c344ec67639dbed7b0c05938ed
Andreas Terstegge <Terstegge@fh-aachen.de>
2023-06-06 07:24:06 +0200

    Fix a problem with C/C++ flags.

--- cmake/CPU/cortex-m0plus.cmake
+++ cmake/CPU/cortex-m0plus.cmake
@@ -5,13 +5,7 @@
 set(YAHAL_CPU "cortex-m0plus")
 
 # Architecture flags
-set(CORTEX_FLAGS "-mcpu=cortex-m0plus -march=armv6s-m -mthumb")
+add_compile_options(-mcpu=cortex-m0plus -march=armv6s-m -mthumb)
 
 # Math flags 
 add_compile_definitions(ARM_MATH_CM0PLUS)
-
-# Add flags
-foreach(LANG IN ITEMS C CXX ASM)
-    set(CMAKE_${LANG}_FLAGS "${CMAKE_${LANG}_FLAGS} ${CORTEX_FLAGS}")
-endforeach()
-
--- cmake/CPU/cortex-m4f.cmake
+++ cmake/CPU/cortex-m4f.cmake
@@ -5,14 +5,9 @@
 set(YAHAL_CPU "cortex-m4f")
 
 # Architecture flags
-set(CORTEX_FLAGS "-mcpu=cortex-m4 -march=armv7e-m -mthumb")
+add_compile_options(-mcpu=cortex-m4 -march=armv7e-m -mthumb)
 
 # FPU flags
-set(CORTEX_FLAGS "${CORTEX_FLAGS} -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mabi=aapcs")
 add_compile_definitions(ARM_MATH_CM4 __FPU_PRESENT)
-
-# Add flags
-foreach(LANG IN ITEMS C CXX ASM)
-    set(CMAKE_${LANG}_FLAGS "${CMAKE_${LANG}_FLAGS} ${CORTEX_FLAGS}")
-endforeach()
+add_compile_options(-mfloat-abi=hard -mfpu=fpv4-sp-d16 -mabi=aapcs)
 
