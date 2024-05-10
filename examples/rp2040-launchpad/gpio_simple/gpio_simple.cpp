3b5b33cba22a39a0fe9b1914c9bd0cd72ff891f1
Andreas Terstegge <Terstegge@fh-aachen.de>
2024-05-10 16:02:11 +0200

    Updated gpio_simple example to use INPUT_INVERT

--- examples/rp2040-launchpad/gpio_simple/gpio_simple.cpp
+++ examples/rp2040-launchpad/gpio_simple/gpio_simple.cpp
@@ -19,6 +19,7 @@
 
 #define LED_RED   13  // left  red LED
 #define SWITCH1   16 // left  S1 button
+#define SWITCH2   17 // right S2 button
 
 int main()
 {
@@ -27,21 +28,25 @@ int main()
 
     // GPIO pins with buttons
     gpio_rp2040_pin s1(SWITCH1); // left button
+    gpio_rp2040_pin s2(SWITCH2); // left button
 
     // Setup GPIO modes. The PULLUP mode is necessary
-    // because there are no external pullup resistors
-    // on the launchpad
+    // because there are NO external pullup resistors
+    // on the launchpad. S1 is automatically inverted,
+    // S2 not.
     red_led. gpioMode(GPIO::OUTPUT);
-    s1.gpioMode(GPIO::INPUT | GPIO::PULLUP);
+    s1.gpioMode(GPIO::INPUT_INVERT | GPIO::PULLUP);
+    s2.gpioMode(GPIO::INPUT        | GPIO::PULLUP);
 
     while(true) {
-        // Set the red and blue LEDs to the
-        // inverted values of button s1 and s2
-        // using the YAHAL gpio methods:
-        red_led.gpioWrite ( ! s1.gpioRead() );
+        // Switch on the red LED if either S1 or S2 is pressed.
+        // Due to operator overloading, the gpio_rp2040_pin object
+        // can be used as a boolean expression (and calls the gpioRead()
+        // method internally).
+        red_led.gpioWrite ( s1.gpioRead() || !s2 );
 
-        // Same as above, but using
-        // (syntactic sugar) C++ operators:
-        red_led  = !s1;
+        // Same as above, but using the object to assign a boolean value
+        // (and call the gpioWrite() method internally)
+        red_led  = s1 || !s2;
     }
 }
