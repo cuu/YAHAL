09c0c464271a162aae255f3952e2680ce4388622
Andreas Terstegge <Terstegge@fh-aachen.de>
2025-02-22 17:24:03 +0100

    Code-reformat. No logic changes.

--- src/MCU/rp2040/adc_rp2040.h
+++ src/MCU/rp2040/adc_rp2040.h
@@ -17,17 +17,21 @@
 #include "adc_interface.h"
 
 class adc_rp2040 : public adc_interface {
-  public:
-	static adc_rp2040 inst;
+public:
+    static adc_rp2040 inst;
 
     // Basic ADC functions
-    void       adcMode       (uint8_t channel, uint16_t mode) override;
-    adc_mode_t getMode       (uint8_t channel) override;
-    uint16_t   adcReadRaw    (uint8_t channel) override;
-    float      adcReadVoltage(uint8_t channel) override;
-    float      rawToVoltage  (uint8_t channel, uint16_t raw) override;
+    void adcMode(uint8_t channel, uint16_t mode) override;
 
-  private:
+    adc_mode_t getMode(uint8_t channel) override;
+
+    uint16_t adcReadRaw(uint8_t channel) override;
+
+    float adcReadVoltage(uint8_t channel) override;
+
+    float rawToVoltage(uint8_t channel, uint16_t raw) override;
+
+private:
 
     adc_rp2040();
 
@@ -39,12 +43,12 @@ class adc_rp2040 : public adc_interface {
 // functionality for a single channel.
 
 class adc_rp2040_channel : public adc_channel {
-  public:
+public:
     adc_rp2040_channel()
-     : adc_channel(adc_rp2040::inst) { }
+            : adc_channel(adc_rp2040::inst) {}
 
     explicit adc_rp2040_channel(uint16_t c)
-     : adc_channel(adc_rp2040::inst, c) { }
+            : adc_channel(adc_rp2040::inst, c) {}
 };
 
 #endif // _ADC_RP2040_H_
