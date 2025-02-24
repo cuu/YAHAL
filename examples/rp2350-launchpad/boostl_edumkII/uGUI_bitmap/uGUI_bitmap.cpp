0fa5fbedc1764ac9c4b67857b7d812e11db36b6c
Andreas Terstegge <Terstegge@fh-aachen.de>
2025-02-24 15:35:34 +0100

    First EDU MKII examples for RP2350 launchpad.

---
+++ examples/rp2350-launchpad/boostl_edumkII/uGUI_bitmap/uGUI_bitmap.cpp
@@ -0,0 +1,79 @@
+// ---------------------------------------------
+//           This file is part of
+//      _  _   __    _   _    __    __
+//     ( \/ ) /__\  ( )_( )  /__\  (  )
+//      \  / /(__)\  ) _ (  /(__)\  )(__
+//      (__)(__)(__)(_) (_)(__)(__)(____)
+//
+//     Yet Another HW Abstraction Library
+//      Copyright (C) Andreas Terstegge
+//      BSD Licensed (see file LICENSE)
+//
+// ---------------------------------------------
+//
+// A simple example for displaying bitmap images
+// with uGUI. Starting point was a 128x128 PNG
+// image (angry-bird.png), which was converted with
+// the png2cpp tool (see /home/student/bin folder).
+// This PNG image has a alpha-channel for displaying
+// transparent areas, like for example the background.
+// So the png2cpp tool was given a background color,
+// in this case 0x00ff00 (green). The generated file
+// is angry-bird.cpp.
+//
+// The image data declaration has to be copied to the
+// file where the data is accessed/needed (see below).
+//
+// After that, the SPI interface to the LCD display is
+// set up, and the LCD Driver and uGUI are started on
+// top. See the doc folder in YAHAL/src/uGUI for more
+// information on the uGUI API.
+
+#include "boostxl_eduMKII.h"
+#include "gpio_rp2350.h"
+#include "spi_rp2350.h"
+
+#include "st7735s_drv.h"
+#include "uGUI.h"
+#include "task.h"
+
+extern const uint16_t angry_bird[16384];
+
+int main(void) {
+    // Switch on backlight
+    gpio_rp2350 lcd_bl(EDU_LCD_BL);
+    lcd_bl.gpioMode(GPIO::OUTPUT | GPIO::INIT_HIGH);
+
+    // Setup SPI interface
+    gpio_rp2350 lcd_cs (EDU_LCD_CS);
+    spi_rp2350  spi(1, EDU_LCD_MISO, EDU_LCD_MOSI, EDU_LCD_SCLK, lcd_cs);
+    spi.setSpeed(24000000);
+
+    // Setup LCD driver
+    gpio_rp2350 lcd_rst(EDU_LCD_RST);
+    gpio_rp2350 lcd_dc (EDU_LCD_DC);
+    st7735s_drv lcd(spi, lcd_rst, lcd_dc, st7735s_drv::Crystalfontz_128x128);
+
+    // Setup uGUI
+    uGUI gui(lcd);
+
+    // Setup bitmap object
+    uGUI::BMP bmp;
+    bmp.height = 128;
+    bmp.width  = 128;
+    bmp.p      = angry_bird;
+    bmp.bpp    = 16;
+    bmp.colors = BMP_RGB565;
+
+    // Show bitmap image
+    gui.DrawBMP(0, 0, &bmp);
+
+    // uGUI does not support scrolling, because it
+    // has no image buffer to access the current
+    // image data. But the LCD driver has a method
+    // for HW-scrolling...
+    while (1) {
+        lcd.scroll(-1);
+        task::sleep_ms(10);
+    }
+}
